79fc77006de2994f253a27e9fd202e62aa84a5e2
GatlaSowmya Gatla <gatlasowmya8@gmail.com>
2023-12-13 05:00:36 +0000

    update

--- hello-world/src/activities.ts
+++ hello-world/src/activities.ts
@@ -1,5 +1,8 @@
+import { error } from "console";
+
 // @@@SNIPSTART typescript-hello-activity
 export async function greet(name: string): Promise<string> {
+  throw new Error('oops')
   return `Hello, ${name}!`;
 }
 // @@@SNIPEND
